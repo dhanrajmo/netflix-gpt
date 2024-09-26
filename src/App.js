1686d4544aa5fec852fa506fdb8e730b3cb70bef
Dhanraj Mohite <dhanrajmohite98@gmail.com>
2024-09-27 02:57:41 +0530

    added latest changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+// import React from 'react';
 import Body from './components/Body.js'
+import appStore from './utils/appStore.js';
+import { Provider } from 'react-redux';
+
+
 function App() {
   return (
-    <Body />
+    <Provider store = {appStore}>
+      <Body />
+    </Provider>
   );
 }
 
