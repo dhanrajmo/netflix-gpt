1003d557243308fa807eec06e2ad8538846e2676
Dhanraj Mohite <dhanrajmohite98@gmail.com>
2024-10-01 04:42:36 +0530

    done

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -1,8 +1,8 @@
 import OpenAI from 'openai';
-import { openAIKey } from './constants';
+// import { openAIKey } from './constants';
 
 export const openai = new OpenAI({
-  apiKey: openAIKey,
+  apiKey: "sk-WmtLog3XSkAC71JTydoDobjm63DakZ_kCVb_P5tVaOT3BlbkFJhd6BqQYDbL1J9626ug1Is9PuDytNKMS9C1QN7YecMA",
   dangerouslyAllowBrowser: true // This is the default and can be omitted
 });
 
