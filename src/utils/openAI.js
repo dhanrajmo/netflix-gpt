b863f32f9e02a769ca71d6632d62e5444a213e13
Dhanraj Mohite <dhanrajmohite98@gmail.com>
2024-10-01 04:06:54 +0530

    name changed

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 // import { OPEN_AI_KEY } from './constants';
 
 export const client = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI_KEY,
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true // This is the default and can be omitted
 });
 
