8f23ba56b45b37de2caf58e2cc0afc40d5af3e72
Dhanraj Mohite <dhanrajmohite98@gmail.com>
2026-05-05 12:50:04 +0530

    changes

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -5,7 +5,7 @@ const SecondaryContainer = () => {
   const movies = useSelector(store => store.movies);
   return (
     <div className='bg-black overflow-x-hidden'>
-      <div className='mt-0 md:-mt-48 relative z-20 pl-2 md:pl-12'>
+      <div className='mt-0 md:-mt-48 z-20 pl-2 md:pl-12'>
       <MovieList title = {'Now Playing'} movies = {movies.nowPlayingMovies}/>
       <MovieList title = {'Popular Movies'} movies = {movies.popularMovies}/>
       <MovieList title = {'Top Rated'} movies = {movies.topRatedMovies}/>
